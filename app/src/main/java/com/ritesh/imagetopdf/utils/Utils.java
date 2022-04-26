659b321b15fa4eb5d04b9746bc404bc3a151c0fd
Ritesh Bhadane <rmbhadane33@gmail.com>
2022-04-26 18:58:55 +0530

    fixed incorrect file size

--- app/src/main/java/com/ritesh/imagetopdf/utils/Utils.java
+++ app/src/main/java/com/ritesh/imagetopdf/utils/Utils.java
@@ -27,10 +27,10 @@ public class Utils {
         float size = fileSize.floatValue();
         if(fileSize < 1000) {
             name = "B";
-        } else if(fileSize < 10e6) {
+        } else if(fileSize < 1000000) {
             name = "kB";
             size /= 1000f;
-        } else if(fileSize < 10e9) {
+        } else if(fileSize < 1000000000) {
             name = "MB";
             size /= 1000000f;
         } else {
@@ -39,29 +39,6 @@ public class Utils {
         }
         return String.format(Locale.getDefault(),"%.1f %s",size, name);
     }
-
-// --Commented out by Inspection START (12-04-2022 09:56):
-//    public static boolean isEncrypted(String path) throws IOException{
-//        PDDocument document = PDDocument.load(new File(path),"1");
-//        boolean encrypted = document.isEncrypted();
-//        document.close();
-//        return encrypted;
-//    }
-// --Commented out by Inspection STOP (12-04-2022 09:56)
-
-// --Commented out by Inspection START (12-04-2022 09:56):
-//    public static Bitmap getPdfThumbnail(String path, int width) throws IOException {
-//        PdfRenderer pdfRenderer = new PdfRenderer(ParcelFileDescriptor.open(new File(path),ParcelFileDescriptor.MODE_READ_ONLY));
-//        Bitmap bitmap;
-//        PdfRenderer.Page page = pdfRenderer.openPage(0);
-//        Pair<Integer, Integer> dimen = getScaledDimensions(page.getWidth(), page.getHeight(),width * 2, width * 2);
-//        bitmap = Bitmap.createBitmap(dimen.first, dimen.second, Bitmap.Config.ARGB_8888);
-//        page.render(bitmap,null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
-//        page.close();
-//        pdfRenderer.close();
-//        return bitmap;
-//    }
-// --Commented out by Inspection STOP (12-04-2022 09:56)
 
     public static Pair<Integer, Integer> getScaledDimensions(int w, int h, int pageWidth, int pageHeight) {
         float ratio = Math.min((float) pageHeight / (float) h, (float) pageWidth / (float) w);
